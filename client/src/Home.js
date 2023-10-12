d539a687960ff1b1a9099acc09a46f36cc5a008d
Marx-wrld <iongonga@kabarak.ac.ke>
2023-10-12 11:56:20 +0300

    UI homepage build

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Loading from "./Loading";
 
 const Home = () => {
@@ -43,7 +43,61 @@ const Home = () => {
                     onChange={(e) => setFullName(e.target.value)}
                     required
                 />
+                <div className="nestedContainer">
+                    <div>
+                        <label htmlFor="currentPosition">Current Position</label>
+                        <input 
+                            type="text" 
+                            name="currentPosition"
+                            className="currentInput"
+                            value={currentPosition}
+                            onChange={(e) => setCurrentPosition(e.target.value)}    
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="currentLength">
+                            For how long? (years from)
+                        </label>
+                        <input 
+                            type="text" 
+                            name="currentLength"
+                            className="currentInput"
+                            value={currentLength}
+                            onChange={(e) => setCurrentLength(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="currentTechnologies">
+                            Technologies used
+                        </label>
+                        <input 
+                            type="text" 
+                            name="currentTechnologies"
+                            className="currentInput"
+                            value={currentTechnologies}
+                            onChange={(e) => setCurrentTechnologies(e.target.value)}
+                            required
+                        />
+                    </div>
+                </div>
+                <label htmlFor="photo">
+                    Upload your headshot image
+                </label>
+                <input 
+                    type="text" 
+                    name="photo"
+                    id="photo"
+                    accept="image/x-png,image/x-jpeg,image/jpg"
+                    onChange={(e) => setHeadshot(e.target.files[0])}
+                    required
+                />
+
+                <button>CREATE RESUME</button>
             </form>
         </div>
-    )
-}
+    );
+};
+
+export default Home;
